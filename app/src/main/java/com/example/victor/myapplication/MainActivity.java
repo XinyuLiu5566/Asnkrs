807d262957310fbda2364c305c7d44f0f11e5d77
Haixuan Chen <haixuan2@illinois.edu>
2018-12-02 15:55:18 -0600

    version 1.0

---
+++ app/src/main/java/com/example/victor/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.victor.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton but1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        but1 = findViewById(R.id.but1);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
