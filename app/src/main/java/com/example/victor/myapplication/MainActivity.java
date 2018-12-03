7758fd7a3a834500203d9f4efa6c9522fb46922d
liuxinyu <xinyu6@illinois.edu>
2018-12-03 11:22:02 -0600

    botton

--- app/src/main/java/com/example/victor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/victor/myapplication/MainActivity.java
@@ -12,6 +12,14 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton but1;
+    private ImageButton but2;
+    private ImageButton but3;
+    private ImageButton but4;
+    private ImageButton but5;
+    private ImageButton but6;
+    private ImageButton but7;
+    private ImageButton but8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +30,73 @@ public class MainActivity extends AppCompatActivity {
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
+
+        but2 = findViewById(R.id.but2);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        but3 = findViewById(R.id.but3);
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main4Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        but4 = findViewById(R.id.but4);
+        but4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main5Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        but5 = findViewById(R.id.but5);
+        but5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main6Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        but6 = findViewById(R.id.but6);
+        but6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main7Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        but7 = findViewById(R.id.but7);
+        but7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main8Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        but8 = findViewById(R.id.but8);
+        but8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main9Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
