63a38910b9da3b98b6db6ba3f4abccc8f177aa8f
liuxinyu <xinyu6@illinois.edu>
2018-12-09 18:37:29 -0600

    111

--- app/src/main/java/com/example/victor/myapplication/Main19Activity.java
+++ app/src/main/java/com/example/victor/myapplication/Main19Activity.java
@@ -2,6 +2,7 @@ package com.example.victor.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Main19Activity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Main19Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main19);
+        WebView webView = (WebView)
+                findViewById(R.id.web19);
+        webView.loadUrl("https://www.youtube.com/watch?v=fONxIK8KPPA");
     }
 }
